2f48c118a56c1aa973603cafaedba423dc743cb4
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-07-31 14:57:01 -0300

    ajusto estilos

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,11 @@ import Members from './components/Members';
 import Resources from './components/Resources';
 import Challenges from './components/Challenges';
 
+import './index.css';
+
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Title isMainTitle>HaciaReact ⚛</Title>
       <Paragraph>
         Basado en el routemap de React:
--- src/components/Link/index.jsx
+++ src/components/Link/index.jsx
@@ -6,7 +6,7 @@ import './styles.css';
 function Link(props) {
   const { children, to } = props;
   return (
-    <a href={to} target="_blank" rel="noreferrer">
+    <a className="link" href={to} target="_blank" rel="noreferrer">
       {children}
     </a>
   );
